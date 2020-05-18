e75d55de25a1df2d86d5c44438f625d43c663240
karthi07 <itkarthi02@gmail.com>
2020-05-18 19:30:47 +0530

    remove password from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_124441) do
+ActiveRecord::Schema.define(version: 2020_05_18_135551) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
