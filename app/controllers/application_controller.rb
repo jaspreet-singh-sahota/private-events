c1bd48e4cea756834672701700a4fe1441bcf627
karthi07 <itkarthi02@gmail.com>
2020-05-19 22:46:20 +0530

    add the bootstap template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :sign_in?
+
   def current_user 
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
