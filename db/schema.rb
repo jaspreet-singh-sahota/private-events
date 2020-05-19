3947747f656ec1b94567598a3fc9f0240187d666
karthi07 <itkarthi02@gmail.com>
2020-05-19 15:26:55 +0530

    test event association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_083326) do
+ActiveRecord::Schema.define(version: 2020_05_19_092958) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_19_083326) do
     t.datetime "event_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
