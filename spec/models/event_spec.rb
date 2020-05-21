e630b558db51bf1449554d5daf42b1a1ceefe8e0
karthi07 <itkarthi02@gmail.com>
2020-05-21 16:33:23 +0530

    add Event model unit test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_length_of(:title).is_at_least(3).is_at_most(25)   }
+    it { should validate_length_of(:description).is_at_least(3).is_at_most(1000)   }
+  end
+
+  describe 'association' do
+    it { should have_many(:invitations) }
+    it { should have_many(:attendees).through(:invitations) }
+    it { should belong_to(:creator) }
+  end
+
 end
